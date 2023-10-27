1cd70af986a3ef47378e9891b95efd167d924ba7
77842st <145762633+77842st@users.noreply.github.com>
2023-10-27 13:57:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,11 @@ date_default_timezone_set('Europe/Helsinki');
 $t=time();
 echo(date("Y-m-d h:i:s a",$t));
 ?>
+
+<?php  
+$name=$_POST["name"];
+$surname=$_POST["surname"];
+$date = date('Y-m-d', $_POST['date']);
+
+echo "$name, $surname, $date";  
+?> 
